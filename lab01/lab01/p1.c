60b2e7608fc9b80a3ce9599f7c25b3e6c7ff2d99
vasilyatsishin <mr.yatsishin@gmail.com>
2023-09-09 14:25:03 +0300

    Add project files.

---
+++ lab01/lab01/p1.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+	float x;
+	float y;
+	float z;
+	printf("Type your ints:");
+	scanf("%f %f %f", &x, &y, &z);
+	float disc = pow(y, 2) - (4 * x * z);
+	if (disc < 0)
+	{
+		printf("This equation has no solutions");
+	}	
+	else if (disc == 0) {
+		float solution = -y / (2 * x);
+		printf("There is one solution: %f", solution);
+	}
+	else {
+		float solution1 = (-y + sqrt(disc)) / (2 * x);
+		float solution2 = (-y - sqrt(disc)) / (2 * x);
+		printf("There are two solutions: %f, %f", solution1, solution2);
+	}
+
+	return 0;
+}
