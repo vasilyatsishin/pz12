afad666ab9f8d5873c490b35f501c95d5b9cebe3
Vasyl Yatsyshyn <mr.yatsishin@gmail.com>
2023-09-20 00:02:55 +0300

    black-var

---
+++ lab02/p1.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int number;
+	int first, second;
+	printf("Type your number: ");
+	scanf("%d", &number);
+		first = number / 10;
+		second = number % 10;
+		int doubleRes = 2 * first * second;
+		if (doubleRes == number)
+		{
+			printf("Your number %d is correct", number);
+		}
+		else if (number < 10 || number > 99) {
+			printf("Your number has to be between 10 & 100");
+		}
+		else
+		{
+			printf("Please, try another number");
+		}
+	return 0;
+}
---
+++ lab02/p2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+/*
+// Функція для обчислення ln(x+1) за допомогою ряду Тейлора
+double taylorSeries(double x, int n) {
+    double result = 0.0;
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0) {
+            result -= pow(x, i) / i;
+        }
+        else {
+            result += pow(x, i) / i;
+        }
+    }
+    return result;
+}
+
+int main() {
+    double A = -0.999999999;  // Початок відрізку
+    double B = 1.0;  // Кінець відрізку
+    int numPoints = 10; // Кількість точок для табуляції
+    int n = 5; // Рівень розкладу у ряд Тейлора
+
+    printf("x\t\tTaylor\t\tExact\n");
+
+    // Протабулюємо функцію та порівняємо з точним значенням
+    for (int i = 0; i <= numPoints; i++) {
+        double x = A + (B - A) * i / numPoints;
+        double taylorValue = taylorSeries(x, n);
+        double exactValue = log(x + 1); // Значення функції, обчислене формулою
+
+        printf("%f\t%f\t%f\n", x, taylorValue, exactValue);
+    }
+
+    return 0;
+} */
