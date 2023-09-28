6650a519a862b2e75b00199cb5649a192a6b8d33
Vasyl Yatsyshyn <mr.yatsishin@gmail.com>
2023-09-28 20:35:49 +0300

    bugs

--- lab02/p1.c
+++ lab02/p1.c
@@ -6,19 +6,19 @@ int main(void) {
 	int first, second;
 	printf("Type your number: ");
 	scanf("%d", &number);
-		first = number / 10;
-		second = number % 10;
-		int doubleRes = 2 * first * second;
-		if (doubleRes == number)
-		{
-			printf("Your number %d is correct", number);
-		}
-		else if (number < 10 || number > 99) {
-			printf("Your number has to be between 10 & 100");
-		}
-		else
-		{
-			printf("Please, try another number");
-		}
+	first = number / 10;
+	second = number % 10;
+	int doubleRes = 2 * first * second;
+	if (doubleRes == number)
+	{
+		printf("Your number %d is correct", number);
+	}
+	else if (number < 10 || number > 99) {
+		printf("Your number has to be between 10 & 100");
+	}
+	else
+	{
+		printf("Please, try another number");
+	}
 	return 0;
 }
